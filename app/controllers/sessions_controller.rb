ac7bba5d07e641c5cd98dc08b96a9797ee03f9dc
RagtagTurtle <e.russell1792@gmail.com>
2018-05-30 21:32:15 -0500

    added contact groups

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,32 +3,32 @@ class SessionsController < ApplicationController
     render 'new.html.erb'
   end
 
-    def create
-      client_params = {
-                       auth: {
-                              email: params[:email],
-                              password: params[:password]
-                             }
-                       }     
+  def create
+    client_params = {
+                     auth: {
+                            email: params[:email],
+                            password: params[:password]
+                           }
+                     }     
 
-      response = Unirest.post(
-                              "http://localhost:3000/user_token", 
-                              parameters: client_params
-                              )
-      
-      if response.code == 201
-        session[:jwt] = response.body["jwt"]
-        flash[:success] = 'Successfully logged in!'
-        redirect_to '/'
-      else
-        flash[:warning] = 'Invalid email or password!'
-        redirect_to '/login'
-      end
-    end
-
-    def destroy
-      session[:jwt] = nil
-      flash[:success] = 'Successfully logged out!'
+    response = Unirest.post(
+                            "http://localhost:3000/user_token", 
+                            parameters: client_params
+                            )
+    
+    if response.code == 201
+      session[:jwt] = response.body["jwt"]
+      flash[:success] = 'Successfully logged in!'
+      redirect_to '/'
+    else
+      flash[:warning] = 'Invalid email or password!'
       redirect_to '/login'
     end
+  end
+
+  def destroy
+    session[:jwt] = nil
+    flash[:success] = 'Successfully logged out!'
+    redirect_to '/login'
+  end
 end
